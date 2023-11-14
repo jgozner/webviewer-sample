50ce4584c2cdfa81ae0fa85c3c524a381864c712
jgozner <jgozner@outlook.com>
2023-11-14 11:00:42 -0800

    Update App.js

--- src/App.js
+++ src/App.js
@@ -4,86 +4,100 @@ import WebViewer from '@pdftron/webviewer';
 
 function App() {
   const viewer = useRef(null);
-  const [instance, setInstance] = useState(null);
-  const [resultsList, setResultsList] = useState([]);
 
-  const addAnnotationsUsingSearchResult = (results) => {
-    const { documentViewer, Annotations } = instance.Core;
-    const annotationManager = documentViewer.getAnnotationManager();
-    const redactAnnotations = results.map((r) => {
-      const annotation = new Annotations.RedactionAnnotation();
-      annotation.PageNumber = r.page_num;
-      annotation.Quads = r.quads.map((quad) => quad.getPoints());
-      annotation.StrokeColor = new Annotations.Color(0, 255, 0);
-      annotation.setContents(r.result_str);
-      annotation.type = r.type;
-      annotation.Author = 'Guest';
-      annotation.setCustomData(
-        'trn-annot-preview',
-        documentViewer.getSelectedText(annotation.PageNumber)
-      );
-      return annotation;
-    });
-    annotationManager.addAnnotations(redactAnnotations);
-    annotationManager.drawAnnotationsFromList(redactAnnotations);
-  };
-
-  const uiSearch = useCallback(
-    (searchValue, options, results) => {
-      if (results.length > 0) {
-        //check first if search value matches a search sample
-        let searchLabel;
-
-        if (searchLabel) {
-          searchLabel = searchLabel.label;
-        } else {
-          searchLabel =
-            '"' +
-            searchValue.replace(/\s+/g, ' ') +
-            '" (' +
-            results.length +
-            ')';
+  const runScript = async (PDFNet, docPath) => {
+    try {
+        const doc = await PDFNet.PDFDoc.createFromURL(docPath);
+        //creating document
+        doc.initSecurityHandler();
+        doc.lock();
+  
+        //setting text search parameters
+        const txtSearch = await PDFNet.TextSearch.create();
+  
+        //mode should be regex to match all instances of multiple words
+        //and also highlight to get highlight information (quads)
+        const mode = PDFNet.TextSearch.Mode.e_reg_expression + PDFNet.TextSearch.Mode.e_highlight;
+  
+        //pattern to search for using regex. Search for multiple strings with
+        //format 'the|cat|Jackson'.
+        const pattern = 'PDF';
+  
+        //the array holding all of the redaction info for each word to be redacted
+        const redactionArray = [];
+  
+        //starting the text search
+        txtSearch.begin(doc, pattern, mode, -1, -1);
+  
+        //call Run() iteratively to find all matching instances of the words
+        while (true) {
+            const result = await txtSearch.run();
+            let hlts;
+  
+  
+            if (result.code === PDFNet.TextSearch.ResultCode.e_found) {
+  
+                //get highlights information from text (for quads)
+                hlts = result.highlights;
+  
+                //begin text search
+                await hlts.begin(doc);
+  
+                //loops through all strings to redact, pushing quad info and page number to redaction array
+                while (await hlts.hasNext()) {
+                    const curPage = await doc.getPage(await hlts.getCurrentPageNumber());
+                    const quadArr = await hlts.getCurrentQuads();
+                    //turns quads into data needed to create RECT object for redaction annotations
+                    for (let i = 0; i < quadArr.length; ++i) {
+                        const currQuad = quadArr[i];
+                        const x1 = Math.min(Math.min(Math.min(currQuad.p1x, currQuad.p2x), currQuad.p3x), currQuad.p4x);
+                        const x2 = Math.max(Math.max(Math.max(currQuad.p1x, currQuad.p2x), currQuad.p3x), currQuad.p4x);
+                        const y1 = Math.min(Math.min(Math.min(currQuad.p1y, currQuad.p2y), currQuad.p3y), currQuad.p4y);
+                        const y2 = Math.max(Math.max(Math.max(currQuad.p1y, currQuad.p2y), currQuad.p3y), currQuad.p4y);
+  
+                        const overlayfillcolor = await PDFNet.ColorPt.init(1,0,0);
+                        const redaction = await PDFNet.RedactionAnnot.create(doc, await PDFNet.Rect.init(x1, y1, x2, y2));
+                        redaction.setColor(overlayfillcolor, 3);
+                        redaction.setInteriorColor(overlayfillcolor, 3);
+                        redaction.setOpacity(.5);
+                        redaction.refreshAppearance();
+  
+                        await curPage.annotPushBack(redaction);
+                    }
+                    await hlts.next();
+                }
+            } else if (result.code === PDFNet.TextSearch.ResultCode.e_done) {
+                break;
+            }
         }
-
-        // see if label is already in the list
-        if (resultsList.some((r) => r.label === searchLabel)) {
-          return;
-        }
-        addAnnotationsUsingSearchResult(results);
-      }
-    },
-    [resultsList]
-  );
-
-  //attach search
-  useEffect(() => {
-    if (instance) {
-      instance.UI.addSearchListener(uiSearch);
-      return () => instance.UI.removeSearchListener(uiSearch);
+  
+        return doc;
+  
+  
+    } catch (err) {
+        console.log(err);
     }
-  }, [uiSearch, instance, resultsList]);
+  }
 
   useEffect(() => {
     WebViewer(
       {
         path: '/webviewer/lib',
-        initialDoc: '/files/WebviewerDemoDoc.pdf',
-        licenseKey: "demo:1688745488452:7c640dad0300000000ff98c75e9e3a6477a0d966fddd63ac8543da906b",
         fullAPI: true
       },
       viewer.current,
-    ).then((instance) => {
-      setInstance(instance);
+    ).then(async (instance) => {
 
-      const { documentViewer } = instance.Core;
+      const { documentViewer, Search, Annotations, annotationManager  } = instance.Core;
 
       instance.UI.enableFeatures(['Redaction']);
       instance.UI.setToolbarGroup('toolbarGroup-Redact');
       instance.UI.setToolMode('AnnotationCreateRedaction');
 
-      documentViewer.addEventListener("documentLoaded", () => {
-        setResultsList([]);
-      })
+      await instance.Core.PDFNet.initialize();
+      instance.UI.openElement('loadingModal')
+      const docInput = await runScript(instance.Core.PDFNet, 'http://localhost:3000/files/WebviewerDemoDoc.pdf');
+      instance.UI.loadDocument(docInput);
 
     });
   }, []);
