1870c1b9ff87724158d83a1b0036077ea1fed090
jgozner <jgozner@outlook.com>
2023-11-14 10:37:47 -0800

    Added redaction sample

--- src/App.js
+++ src/App.js
@@ -1,10 +1,67 @@
 import './App.css';
-import { useEffect, useRef, useState } from 'react';
+import { useCallback, useEffect, useRef, useState } from 'react';
 import WebViewer from '@pdftron/webviewer';
 
 function App() {
   const viewer = useRef(null);
   const [instance, setInstance] = useState(null);
+  const [resultsList, setResultsList] = useState([]);
+
+  const addAnnotationsUsingSearchResult = (results) => {
+    const { documentViewer, Annotations } = instance.Core;
+    const annotationManager = documentViewer.getAnnotationManager();
+    const redactAnnotations = results.map((r) => {
+      const annotation = new Annotations.RedactionAnnotation();
+      annotation.PageNumber = r.page_num;
+      annotation.Quads = r.quads.map((quad) => quad.getPoints());
+      annotation.StrokeColor = new Annotations.Color(0, 255, 0);
+      annotation.setContents(r.result_str);
+      annotation.type = r.type;
+      annotation.Author = 'Guest';
+      annotation.setCustomData(
+        'trn-annot-preview',
+        documentViewer.getSelectedText(annotation.PageNumber)
+      );
+      return annotation;
+    });
+    annotationManager.addAnnotations(redactAnnotations);
+    annotationManager.drawAnnotationsFromList(redactAnnotations);
+  };
+
+  const uiSearch = useCallback(
+    (searchValue, options, results) => {
+      if (results.length > 0) {
+        //check first if search value matches a search sample
+        let searchLabel;
+
+        if (searchLabel) {
+          searchLabel = searchLabel.label;
+        } else {
+          searchLabel =
+            '"' +
+            searchValue.replace(/\s+/g, ' ') +
+            '" (' +
+            results.length +
+            ')';
+        }
+
+        // see if label is already in the list
+        if (resultsList.some((r) => r.label === searchLabel)) {
+          return;
+        }
+        addAnnotationsUsingSearchResult(results);
+      }
+    },
+    [resultsList]
+  );
+
+  //attach search
+  useEffect(() => {
+    if (instance) {
+      instance.UI.addSearchListener(uiSearch);
+      return () => instance.UI.removeSearchListener(uiSearch);
+    }
+  }, [uiSearch, instance, resultsList]);
 
   useEffect(() => {
     WebViewer(
@@ -17,6 +74,17 @@ function App() {
       viewer.current,
     ).then((instance) => {
       setInstance(instance);
+
+      const { documentViewer } = instance.Core;
+
+      instance.UI.enableFeatures(['Redaction']);
+      instance.UI.setToolbarGroup('toolbarGroup-Redact');
+      instance.UI.setToolMode('AnnotationCreateRedaction');
+
+      documentViewer.addEventListener("documentLoaded", () => {
+        setResultsList([]);
+      })
+
     });
   }, []);
 
