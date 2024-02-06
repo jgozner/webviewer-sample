1d89561b8a94d28f6eb86b1fee310786d517ac73
jgozner <jgozner@outlook.com>
2024-02-06 11:13:17 -0800

    Added download sample

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import './App.css';
 import { useEffect, useRef, useState } from 'react';
 import WebViewer from '@pdftron/webviewer';
+import { saveAs } from 'file-saver';
 
 function App() {
   const viewer = useRef(null);
@@ -10,13 +11,33 @@ function App() {
     WebViewer(
       {
         path: '/webviewer/lib',
-        initialDoc: '/files/WebviewerDemoDoc.pdf',
+        initialDoc: '/files/legal-contract.docx',
         licenseKey: "demo:1688745488452:7c640dad0300000000ff98c75e9e3a6477a0d966fddd63ac8543da906b",
         fullAPI: true
       },
       viewer.current,
     ).then((instance) => {
       setInstance(instance);
+
+      const { documentViewer, annotationManager } = instance.Core;
+
+      instance.UI.setHeaderItems(header => {
+        header.push({
+          type: 'actionButton',
+          img: '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path d="M0 0h24v24H0z" fill="none"/><path d="M17 3H5c-1.11 0-2 .9-2 2v14c0 1.1.89 2 2 2h14c1.1 0 2-.9 2-2V7l-4-4zm-5 16c-1.66 0-3-1.34-3-3s1.34-3 3-3 3 1.34 3 3-1.34 3-3 3zm3-10H5V5h10v4z"/></svg>',
+          onClick: async () => {
+            const doc = documentViewer.getDocument();
+            const data = await doc.getFileData({
+              // saves the document with annotations in it
+              downloadType: 'office'
+            });
+            const arr = new Uint8Array(data);
+            const blob = new Blob([arr], { type: 'application/vnd.openxmlformats-officedocument.wordprocessingml.document' });
+            saveAs(blob, "legal-contract.docx");
+          }
+        });
+      });
+
     });
   }, []);
 
