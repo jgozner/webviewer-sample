bf354a70d5f98d746536d30e014300db374aedf2
jgozner <jgozner@outlook.com>
2024-05-01 19:36:31 -0700

    Updated sample

--- src/App.js
+++ src/App.js
@@ -6,9 +6,7 @@ function App() {
   const viewer = useRef(null);
   const [instance, setInstance] = useState(null);
 
-  const configData = {
-    LOCKED_PAGES: [2,3,4]
-  };
+  const LOCKED_PAGES = [2,3,4];
 
   useEffect(() => {
     WebViewer(
@@ -17,12 +15,52 @@ function App() {
         initialDoc: '/files/WebviewerDemoDoc.pdf',
         licenseKey: "demo:1688745488452:7c640dad0300000000ff98c75e9e3a6477a0d966fddd63ac8543da906b",
         fullAPI: true,
-        custom: JSON.stringify(configData),
-        config: "config.js"
+        //custom: JSON.stringify({LOCKED_PAGES}), We can also set this to be executed in the config file
+        //config: "config.js"
       },
       viewer.current,
     ).then((instance) => {
       setInstance(instance);
+
+      const { documentViewer, Tools } = instance.Core;
+      
+      // Cache original mouse event functions
+      Tools.RectangleCreateTool.prototype.__mouseLeftDown = Tools.RectangleCreateTool.prototype.mouseLeftDown;
+      Tools.LineCreateTool.prototype.__mouseLeftDown = Tools.LineCreateTool.prototype.mouseLeftDown;
+      
+      function getPageCoor(e) {
+        var tool = documentViewer.getToolMode();
+        var loc = tool.getMouseLocation(e)
+        var pageRange = documentViewer.getDisplayModeManager().getDisplayMode().getSelectedPages(loc, loc);
+        var currPageIndex = (pageRange.first === null) ? pageRange.last : pageRange.first;
+        
+        if(!currPageIndex){ 
+          return null;
+        }
+        return documentViewer.getDisplayModeManager().getDisplayMode().windowToPage(loc, currPageIndex);
+      }
+      
+      //RectangleCreateTool 
+      Tools.RectangleCreateTool.prototype.mouseLeftDown = function(e) {
+        var coor = getPageCoor(e)
+        // If pageNumber matches, stop propagating the event.
+        if (coor && LOCKED_PAGES.indexOf(coor.pageNumber) > -1) {
+          return;
+        };
+        return Tools.RectangleCreateTool.prototype.__mouseLeftDown.apply(this, arguments);
+      }
+
+      //Rectagle annotation 
+      Tools.LineCreateTool.prototype.mouseLeftDown = function(e) {
+        var coor = getPageCoor(e)
+        // If pageNumber matches, stop propagating the event.
+        if (coor && LOCKED_PAGES.indexOf(coor.pageNumber) > -1) {
+          return;
+        };
+        return Tools.LineCreateTool.prototype.__mouseLeftDown.apply(this, arguments);
+      }
+
+      //Will need to do the following for all the other tools.
 
     });
   }, []);
