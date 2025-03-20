e324497acb067f8029762f5d16d9c33d0941155c
jgozner <jgozner@outlook.com>
2025-03-19 17:13:36 -0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -4,24 +4,47 @@ import WebViewer from '@pdftron/webviewer';
 
 function App() {
   const viewer = useRef(null);
-  const [instance, setInstance] = useState(null);
+  const wvInstace = useRef();
+
+  const switchTool = (toolName) => {
+    const { documentViewer, Tools } = wvInstace.current.Core;
+    let tool;
+    switch(toolName){
+      case "FREEHAND":
+        tool = documentViewer.getTool(Tools.ToolNames.FREEHAND);
+        break;
+      case "CALLOUT":
+        tool = documentViewer.getTool(Tools.ToolNames.CALLOUT);
+        break;
+      case "FREETEXT":
+        tool = documentViewer.getTool(Tools.ToolNames.FREETEXT);
+        break;
+    }
+    documentViewer.setToolMode(tool);
+  }
 
   useEffect(() => {
     WebViewer(
       {
         path: '/webviewer/lib',
         initialDoc: '/files/WebviewerDemoDoc.pdf',
-        licenseKey: "demo:1688745488452:7c640dad0300000000ff98c75e9e3a6477a0d966fddd63ac8543da906b",
-        fullAPI: true
       },
       viewer.current,
-    ).then((instance) => {
-      setInstance(instance);
+    ).then(async (instance) => {
+      wvInstace.current = instance;
+      instance.UI.disableElements([
+        'default-top-header'
+      ]);
     });
   }, []);
 
   return (
     <div className="App">
+      <div>
+        <button onClick={() => switchTool("FREEHAND")}>Pencil</button>
+        <button onClick={() => switchTool("CALLOUT")}>Callout</button>
+        <button onClick={() => switchTool("FREETEXT")}>Free Text</button>
+      </div>
       <div className="webviewer" ref={viewer}></div>
     </div>
   );
