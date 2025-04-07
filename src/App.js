bde6426cb384a882508740170229dc09cec46827
jgozner <jgozner@outlook.com>
2025-04-07 17:23:55 -0400

    Added code sample for headless docx -> pdf

--- src/App.js
+++ src/App.js
@@ -7,22 +7,11 @@ function App() {
   const [instance, setInstance] = useState(null);
 
   useEffect(() => {
-    WebViewer(
-      {
-        path: '/webviewer/lib',
-        initialDoc: '/files/WebviewerDemoDoc.pdf',
-        licenseKey: "demo:1688745488452:7c640dad0300000000ff98c75e9e3a6477a0d966fddd63ac8543da906b",
-        fullAPI: true
-      },
-      viewer.current,
-    ).then((instance) => {
-      setInstance(instance);
-    });
+
   }, []);
 
   return (
     <div className="App">
-      <div className="webviewer" ref={viewer}></div>
     </div>
   );
 }
