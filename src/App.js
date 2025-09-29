912fa783448e9d0aeae438e34014d17c8f7733f1
jgozner <jgozner@outlook.com>
2025-09-28 18:56:27 -0700

    updates

--- src/App.js
+++ src/App.js
@@ -4,30 +4,18 @@ import WebViewer from '@pdftron/webviewer';
 
 function App() {
   const viewer = useRef(null);
-<<<<<<< Updated upstream
-  const [instance, setInstance] = useState(null);
-=======
   const wvInstance = useRef();
->>>>>>> Stashed changes
 
   useEffect(() => {
     WebViewer(
       {
         path: '/webviewer/lib',
-<<<<<<< Updated upstream
-        initialDoc: '/files/WebviewerDemoDoc.pdf',
-=======
         enableFilePicker: true,
->>>>>>> Stashed changes
         licenseKey: "demo:1688745488452:7c640dad0300000000ff98c75e9e3a6477a0d966fddd63ac8543da906b",
         fullAPI: true
       },
       viewer.current,
     ).then((instance) => {
-<<<<<<< Updated upstream
-      setInstance(instance);
-    });
-=======
       wvInstance.current = instance;
 
       const { Core, UI } = instance;
@@ -35,6 +23,7 @@ function App() {
 
       UI.addEventListener(UI.Events.MULTI_VIEWER_READY, () => {
         const [documentViewer1, documentViewer2] = Core.getDocumentViewers();
+
         const startCompare = async () => {
           const shouldCompare = documentViewer1.getDocument() && documentViewer2.getDocument();
           if (shouldCompare) { // Check if both documents loaded before comparing
@@ -46,13 +35,23 @@ function App() {
         }
         documentViewer1.addEventListener('documentLoaded', startCompare);
         documentViewer2.addEventListener('documentLoaded', startCompare);
-        documentViewer2.loadDocument('https://www.pdftron.com/compare2.pdf');
+
+        documentViewer1.addEventListener(Core.DocumentViewer.Events.PAGES_UPDATED, (e) =>{
+
+          e.rotationChanged.forEach((page) => {
+            documentViewer1.getRotation()
+            const rotation = documentViewer1.getCompleteRotation(page);
+            documentViewer2.setRotation(rotation)
+          })
+        });
+
+        documentViewer1.loadDocument('files/semantic_1.pdf');
+        documentViewer2.loadDocument('files/semantic_2.pdf');
       });
 
       UI.enableFeatures([UI.Feature.MultiViewerMode]);
       UI.enterMultiViewerMode();
     })
->>>>>>> Stashed changes
   }, []);
 
   return (
